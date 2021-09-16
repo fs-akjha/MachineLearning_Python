2528a5c0714dc57ba639f1bb8ed68333f29a97de
Akash Jha <ajha@fleetstudio.com>
2021-09-16 18:51:09 +0530

    'Normalization with L1 norms'

---
+++ MLStep_by_step/Normalization_L1.py
@@ -0,0 +1,11 @@
+from pandas import read_csv
+from numpy import array, set_printoptions
+from sklearn.preprocessing import Normalizer
+
+path=r"C:\Akash Files\MachineLearning_Python\MLStep_by_step\pima-indians-diabetes.csv"
+dataframe=read_csv(path)
+array=dataframe.values
+Data_normalizer=Normalizer(norm='l1').fit(array)
+Data_normalized=Data_normalizer.transform(array)
+set_printoptions(precision=2)
+print("\n Normalized data \n",Data_normalized[0:3])
