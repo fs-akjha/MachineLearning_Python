e1e7a4722b6e0bd48b84b1668834e93fbe59715b
Akash Jha <ajha@fleetstudio.com>
2021-09-20 19:29:06 +0530

    'Splitting Datasets using Scikit-Learn'

---
+++ MLStep_by_step/scikit-learn_splliting_datasets.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import load_iris
+iris=load_iris()
+X=iris.data
+y=iris.target
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=1)
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
